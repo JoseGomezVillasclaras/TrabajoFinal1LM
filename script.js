f7236deead79880f32f050e286ddcdcc727d8a05
JoseGomezVillasclaras <jose.gomez.villasclaras@gmail.com>
2023-05-15 18:30:15 +0200

    a

--- script.js
+++ script.js
@@ -4,10 +4,33 @@ var modoActual = localStorage.getItem('modo') || 'dia';
 // Obtener el tamaño de fuente actual del almacenamiento local o establecer un valor predeterminado
 var tamañoActual = localStorage.getItem('tamaño') || 'mediano';
 
+// Función para generar colores aleatorios
+function generarColorAleatorio() {
+  var letras = '0123456789ABCDEF';
+  var color = '#';
+
+  for (var i = 0; i < 6; i++) {
+    color += letras[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
 // Función para cambiar el modo
 function cambiarModo() {
   var selectModo = document.getElementById('modo');
   var nuevoModo = selectModo.value;
+
+  if (nuevoModo === 'random') {
+    var colorTexto = generarColorAleatorio();
+    var colorFondo = generarColorAleatorio();
+
+    document.body.style.color = colorTexto;
+    document.body.style.backgroundColor = colorFondo;
+  } else {
+    document.body.style.color = '';
+    document.body.style.backgroundColor = '';
+  }
 
   document.body.classList.remove(modoActual);
   document.body.classList.add(nuevoModo);
@@ -19,23 +42,4 @@ function cambiarModo() {
 // Función para cambiar el tamaño de fuente
 function cambiarTamaño() {
   var selectTamaño = document.getElementById('tamaño');
-  var nuevoTamaño = selectTamaño.value;
-
-  document.body.classList.remove(tamañoActual);
-  document.body.classList.add(nuevoTamaño);
-
-  localStorage.setItem('tamaño', nuevoTamaño);
-  tamañoActual = nuevoTamaño;
-}
-
-// Establecer el modo y tamaño actual en la carga de la página
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.classList.add(modoActual);
-  document.body.classList.add(tamañoActual);
-
-  var selectModo = document.getElementById('modo');
-  selectModo.value = modoActual;
-
-  var selectTamaño = document.getElementById('tamaño');
-  selectTamaño.value = tamañoActual;
-});
+  var nuevoTamaño = select
